98437db108c4d270ddfbc84b29a078ec5c27b600
lhuanyu <lhuanyutot@163.com>
2018-05-01 19:46:55 +0800

    由于chrome等浏览器是由https传输，而请求内容是http，那么浏览器会阻止这种混合内容的网站的请求

--- src/js/dbapi.js
+++ src/js/dbapi.js
@@ -18,15 +18,28 @@ function josnp(){
 // 回调执行函数,异步执行函数在全局作用域下调用所以定义在全局
 function onBack(res) {
     data=res;
-    alert(JSON.stringify(data));
     showImg(data);
+}
+
+function trim(str){ //删除左边的空格
+    return str.replace(/(^\s*)|(\s*$)/g, "");
+}
+
+//给字符串str指定位置position添加字符串str2
+function addHttps(str1,position,str2){
+    trim(str1);
+    var reg=new RegExp("\(\.\{"+position+"\}\)");
+    str2='$1'+str2;
+    return str1.replace(reg, str2);
 }
 
 function showImg(data){
     var movLens=data.subjects.length;
     for(var i=0;i<movLens;i++){
         imageArr[i] = document.createElement('img');
-        imageArr[i].src=data.subjects[i].images.small;
+        //解决chrome和firefox浏览器加载http资源受限的情况
+        var imgSrc=addHttps(data.subjects[i].images.small,4,'s');
+        imageArr[i].src=imgSrc;
         imageArr[i].style.maxHeight="377px";
         imageArr[i].onerror=function(event){
             //由于get不到图片，所以需要给用户提醒一下，哪些失败了
