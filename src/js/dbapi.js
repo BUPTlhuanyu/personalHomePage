debf29ba5a72aadf642607701a20871bffe8f285
lhuanyu <lhuanyutot@163.com>
2018-05-03 22:50:17 +0800

    利用定时器实现并发协作，详解件循环与任务队列

--- src/js/dbapi.js
+++ src/js/dbapi.js
@@ -4,7 +4,6 @@
 
 
 var data=null,db=null;
-var imageArr=[];
 function josnp(){
     //josnp获取豆瓣电影正在热映的json数据
     var script = document.createElement('script');
@@ -35,21 +34,50 @@ function onBack(res) {
 //     return str1.replace(reg, str2);
 // }
 
+//逐个appendchild
+// var imageArr=[];
+// function showImg(data){
+//     var date=new Date();
+//     var movLens=data.subjects.length;
+//     for(var i=0;i<movLens;i++){
+//         imageArr[i] = document.createElement('img');
+//         //一开始为了解决chrome和firefox浏览器加载http资源受限的情况，由于通过https获取的src自动变为https，所以这里不再需要手动添加s了
+//         // var imgSrc=addHttps(data.subjects[i].images.small,4,'s');
+//         imageArr[i].src=data.subjects[i].images.small;
+//         imageArr[i].style.maxHeight="377px";
+//         imageArr[i].onerror=function(event){
+//             //由于get不到图片，所以需要给用户提醒一下，哪些失败了
+//             event.target.style.display="none";
+//         };
+//         db.appendChild(imageArr[i]);
+//     }
+//      console.log(new Date() - date);
+// }
+
+// 利用DocumentFragment，然后append
 function showImg(data){
-    var movLens=data.subjects.length;
+    var fragment=document.createDocumentFragment(),
+        movLens=data.subjects.length,
+        images=null;
+    var date=new Date();
     for(var i=0;i<movLens;i++){
-        imageArr[i] = document.createElement('img');
+        images = document.createElement('img');
         //一开始为了解决chrome和firefox浏览器加载http资源受限的情况，由于通过https获取的src自动变为https，所以这里不再需要手动添加s了
         // var imgSrc=addHttps(data.subjects[i].images.small,4,'s');
-        imageArr[i].src=data.subjects[i].images.small;
-        imageArr[i].style.maxHeight="377px";
-        imageArr[i].onerror=function(event){
+        images.src=data.subjects[i].images.small;
+        //虽然这里只修改一个样式，但是还是应该养成良好的习惯：利用DOM2级样式中style特性下的cssText属性，一次修改多个样式，只需一次回流和重绘
+        // images.style.maxHeight="377px";
+        images.style.cssText +=";max-height:377px";
+        images.onerror=function(event){
             //由于get不到图片，所以需要给用户提醒一下，哪些失败了
             event.target.style.display="none";
         };
-        db.appendChild(imageArr[i]);
+        fragment.appendChild(images);
     }
+    db.appendChild(fragment);
+    console.log(new Date() - date);
 }
+
 window.onload=function(){
     db=document.getElementById('db-cont');
     josnp();
