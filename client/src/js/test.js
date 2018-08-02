22f1d2e079af33986c6789f78182e64d81b11ec0
liaohuanyu_i <liaohuanyu_i@didichuxing.com>
2018-08-02 22:37:03 +0800

    gitignore

--- client/src/js/test.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by lhy on 2018/5/26.
- */
-alert('1');
--- routes/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import express from 'express'
-import BookHandle from '../controller/book'
-const router = express.Router()
-
-router.get('/book', BookHandle.getBooks);
-
-export default router
